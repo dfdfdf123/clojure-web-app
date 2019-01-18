285b2a52c447a628f02f9549f7ff2aae772f6c06
johnromanoff <johnromanoff@yandex.ru>
2019-01-18 23:03:55 +0300

    update hadler

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-core "1.7.1"]                 
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler clojure-web-app.handler/app}
   :profiles
--- src/clojure_web_app/handler.clj
+++ src/clojure_web_app/handler.clj
@@ -1,10 +1,26 @@
 (ns clojure-web-app.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [hiccup.page :refer [doctype include-css]]
+            [hiccup.core :refer [html]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn page [title & content]
+  (str
+    (html
+      (doctype :html5)
+      [:html
+       [:head
+        [:title title]
+        (include-css "main.css")]
+       [:body content]])))
+
+(defn index []
+  (page "Clojure-web-app"
+   [:h1 "Clojure-web-app"]))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (index))
   (route/not-found "Not Found"))
 
 (def app
